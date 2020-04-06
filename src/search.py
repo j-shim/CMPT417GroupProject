20ac1219251b8a9e4fa43af6c1c5eca8d93f27a4
June Shim <jys2@sfu.ca>
2020-04-05 22:29:05 -0700

    Implement BFS (JS)

--- src/search.py
+++ src/search.py
@@ -15,7 +15,7 @@ def depth_first_search(puzzle):
         node = fringe.pop()
 
         if puzzle.is_goal_state(node):
-            return depth_first_search_backtrace(start_state, node, parent)
+            return backtrace(start_state, node, parent)
 
         expanded[str(node)] += 1
 
@@ -26,7 +26,7 @@ def depth_first_search(puzzle):
                 parent[str(state)] = (str(node), action)
 
 
-def depth_first_search_backtrace(start_state, goal_state, parent):
+def backtrace(start_state, goal_state, parent):
     actions = []
     key = str(goal_state)
     while True:
@@ -35,3 +35,31 @@ def depth_first_search_backtrace(start_state, goal_state, parent):
             return actions
         actions.append(parent[key][1])
         key = parent[key][0]
+
+
+def get_cost_of_actions(actions):
+    return len(actions)
+
+
+def breadth_first_search(puzzle):
+    fringe = util.Queue()
+    start_state = puzzle.get_start_state()
+    fringe.push(start_state)
+    in_fringe = util.Counter()
+    in_fringe[str(start_state)] += 1
+    parent = {}
+    while True:
+        if fringe.isEmpty():
+            return None
+
+        node = fringe.pop()
+
+        if puzzle.is_goal_state(node):
+            return backtrace(start_state, node, parent)
+
+        for successor in puzzle.get_successors(node):
+            state, action = successor
+            if in_fringe[str(state)] == 0:
+                fringe.push(state)
+                parent[str(state)] = (str(node), action)
+                in_fringe[str(state)] += 1
